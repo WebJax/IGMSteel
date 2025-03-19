b9c6e726f17cdfbf56ec5d2ee7e6845d9c2c4960
Jaxweb <info@jaxweb.dk>
2025-03-19 15:51:53 +0100

    autocomplete til company name

--- inc/woocommerce/add-company-name.php
+++ inc/woocommerce/add-company-name.php
@@ -3,5 +3,6 @@ add_filter('woocommerce_billing_fields', function($fields) {
     // Sæt firmanavnets prioritet til lige efter efternavn (standard er 30)
     $fields['billing_company']['priority'] = 25;
     $fields['billing_company']['required'] = false; // Gør det valgfrit (eller true hvis det skal være påkrævet)
+    $fields['billing_company']['autocomplete'] = 'organization'; // Hjælper med autofill
     return $fields;
 });
